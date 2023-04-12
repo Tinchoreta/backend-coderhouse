11180b6575b5ede69bea9ab88991bbb22b2d97f3
Martin Reta <tinchoreta@gmail.com>
2023-04-11 23:12:03 -0300

    editEvent y deleteEvent listos

--- handsonlab/hol_02.js
+++ handsonlab/hol_02.js
@@ -75,10 +75,15 @@ class TicketManager{
         let event = this.getEventById(idEvent)
         
         // verifico que exista el evento
-        if(event !== []) {
+        if(event !== [] && event && event.participants) {
             // verifico la capacidad que no esté lleno
             if(event.participants.length < event.capacity){
-                //verifico que no exista el participante
+                //verifico que no exista el participante, 
+                //si el índice es igual a -1 (FindIndex devuelve eso cuándo no lo encuentra) 
+                //quiere decir que no se encontró en el array de participantes
+                //entonces lo agrego
+                
+
                 if (event.participants.findIndex(participant => participant === parseInt(idUser))===-1){
                     event.participants.push(idUser)
                     console.log(event.participants)
@@ -111,6 +116,7 @@ class TicketManager{
             
             event.id = this.getNewId();
             console.log(event)
+            console.log(`Nuevo id ${event.id}`)
             this.events.push(event);
             
         }
@@ -118,20 +124,63 @@ class TicketManager{
             console.error("No se encontró el evento")
         }
     }
+
+    editEvent(idEvent, objEventProps){
+        let event = this.getEventById(idEvent);
+        
+        {
+
+        }
+    }
+
+    deleteEvent(idEvent) {
+        const index = this.events.findIndex(event => event.id === parseInt(idEvent));
+        
+        //en el if pregunto si lo encontró, es decir si findIndex no devolvió -1
+        if (index !== -1) {
+            //borro el evento con splice
+            this.events.splice(index, 1);
+            console.log(`Evento con id ${idEvent} eliminado`);
+        } else {
+            console.error("No se encontró el evento");
+        }
+    }
+
+    editEvent(idEvent, objEventProps){
+        let event = this.getEventById(idEvent);
+        
+        // Verificar que el evento existe
+        if(event !== []) {
+            // Recorro las propiedades del objeto que se le pasa como parámetro
+            //key es la propiedad y value el valor que va a tomar en el evento modificado
+            Object.entries(objEventProps).forEach(([key, value]) => {
+                event[key] = value;
+            });
+            console.log(`El evento modificado es: ${JSON.stringify(event)}`);
+            
+             // Actualizo el evento en el arreglo de eventos
+            const index = this.events.findIndex((e) => e.id === idEvent);
+            
+            //saco el original
+            this.events.splice(index, 1, event);
+            //añado el modificado
+            this.events.push(event);
+            console.log(`El evento con ID ${idEvent} ha sido modificado`);
+            }
+            
+        }
+    
 }
 
-let ticket= new TicketManager()
-ticket.getEvents()
+let ticket = new TicketManager()
+
 ticket.addEvent({name: 'Cirque du soleil', place: 'France', price: 50, capacity: null, date: undefined })
 ticket.addEvent({name: 'Fuerza Bruta', place: 'Argentina', price: 25, capacity: 500, date: new Date('06/09/2023') })
 ticket.addEvent({name: 'Teatro ciego', place: 'República Checa', price: 20, capacity: 3, date: new Date('04/08/2023') })
 
-let evento2 = ticket.getEventById(2)
+ticket.addNewEvent(3, "Ciudad del Cabo", new Date().toDateString())
 
-console.log(evento2)
-
-let evento4 = ticket.getEventById(4)
-console.log(evento4)
+console.log(ticket.getEventById(4))
 
 ticket.addParticipant(3,4)
 ticket.addParticipant(3,4)
@@ -139,4 +188,13 @@ ticket.addParticipant(3,6)
 ticket.addParticipant(3,5)
 ticket.addParticipant(3,7)
 
-ticket.addNewEvent(3, "Ciudad del Cabo", new Date().toDateString())
+ticket.deleteEvent(1)
+ticket.deleteEvent(2)
+ticket.deleteEvent(3)
+ticket.deleteEvent(4)
+
+ticket.getEvents()
+
+ticket.editEvent(2,{name: 'Padel WPT', place: 'San Luis', price: 123, capacity: 500, date: new Date('08/07/2023') })
+console.log(ticket.getEventById(2))
+
