b7149ac9b66e60fff8fd661183fca8c44890ace4
Martin Reta <tinchoreta@gmail.com>
2023-04-16 16:00:32 -0300

    Primer entregable ProductManager con readme.md y pruebas

--- backend/hol_02.js
+++ backend/hol_02.js
@@ -7,107 +7,56 @@ la cuál tendrá un arreglo de Productos que iniciará vacíos.
 */
 
 class ProductManager {
-
     constructor() {
-        this.Products = []
-
+        this.products = [];
     }
 
-    getProducts() {
+    getProducts = () => {
+        return this.products;
+    };
 
-        return this.Products;
-    }
+    addProduct = ({ title, description, price, thumbnail, stock }) => {
+        const newId = this.getLastId() + 1;
+        const newProduct = { id: newId, title, description, price, thumbnail, stock };
+        this.products.push(newProduct);
+        return newId;
+    };
 
-    addProduct({ title, description, price, thumbnail, stock }) {
-
-        let id = 0
-
-        // id autoincremental, si no hay Productos le asigna 1 al id, sino le suma uno al último id creado
-
-        id = this.getNewId();
-
-
-        let Product = { id, title, description, price, thumbnail, stock }
-
-        // console.log(Product)
-
-        this.Products.push(Product)
-
-    }
-
-    getLastId() {
-        if (this.Products.length === 0) {
+    getLastId = () => {
+        const lastIndex = this.products.length - 1;
+        if (lastIndex < 0) {
             return 0;
         }
-        else {
-            let lastID = this.Products[this.Products.length - 1].id
-            return lastID;
+        return this.products[lastIndex].id;
+    };
+
+    getProductById = (idProduct) => {
+        const found = this.products.find((product) => product.id === parseInt(idProduct));
+        if (!found) {
+            throw new Error("Producto no encontrado");
         }
+        return found;
+    };
+
+    editProduct(id, { title, description, price, thumbnail, stock }) {
+        const product = this.getProductById(id);
+        if (!product) {
+            throw new Error(`Producto con ID: ${id} no encontrado`);
+        }
+        product.title = title ?? product.title;
+        product.description = description ?? product.description;
+        product.price = price ?? product.price;
+        product.thumbnail = thumbnail ?? product.thumbnail;
+        product.stock = stock ?? product.stock;
+        return product;
     }
-
-    getNewId() {
-
-        let lastId = this.getLastId();
-        if (lastId === 0) {
-            return 1;
+    deleteProduct = (idProduct) => {
+        const productIndex = this.products.findIndex((product) => product.id === parseInt(idProduct));
+        if (productIndex === -1) {
+            throw new Error(`Producto con ID: ${idProduct} no encontrado`);
         }
-        else {
-
-            return ++lastId;
-        }
-
-    }
-
-    getProductById(idProduct) {
-        const found = this.Products.find(Product => Product.id === parseInt(idProduct))
-        // console.log(JSON.stringify(found) + " search by ID")
-
-        if (found === undefined) {
-            console.log("Not found");
-            return "Not found"; 
-        } else {
-            return found;
-        }
-    }
-
-    editProduct(idProduct, objProductProps) {
-        const modProduct = this.getProductById(idProduct);
-        if (modProduct === "Not found") {
-            console.log(`El Producto con ID ${idProduct} no pudo modificarse porque no se encontró`);
-            return;
-        }
-
-        // Verifico si los parámetros de entrada son válidos
-        const validProps = ["title", "description", "price", "thumbnail", "stock"];
-        const invalidProps = Object.keys(objProductProps).filter(prop => !validProps.includes(prop));
-        if (invalidProps.length > 0) {
-            console.log(`Los siguientes parámetros no son válidos: ${invalidProps.join(", ")}`);
-
-        }
-        // console.log(objProductProps)
-        // Actualizo el objeto del producto
-        Object.assign(modProduct, objProductProps);
-
-        // Actualizo el producto en el arreglo this.Products
-        const index = this.Products.findIndex((e) => e.id === idProduct);
-        this.Products[index] = modProduct;
-
-        console.log(`El Producto con ID ${idProduct} ha sido modificado`);
-
-    }
-
-    deleteProduct(idProduct) {
-        const index = this.Products.findIndex(Product => Product.id === parseInt(idProduct));
-        
-        //en el if pregunto si lo encontró, es decir si findIndex no devolvió -1
-        if (index !== -1) {
-            //borro el Producto con splice
-            this.Products.splice(index, 1);
-            console.log(`Producto con id ${idProduct} eliminado`);
-        } else {
-            console.error("Not found");
-        }
-    }
+        this.products.splice(productIndex, 1);
+    };
 }
 
 let productAdmin = new ProductManager()
@@ -142,8 +91,6 @@ try {
     console.error(error.message);
 }
 
-console.log("\n -------------------");
-
 /*
     Obtiene los productos actualizados con el método getProducts
 */
@@ -161,7 +108,7 @@ try {
     const product = prodAdmin.getProductById(productId);
     console.log("Resultado de: getProductById(1):")
     console.log(product); // { id: 1, ...}
-    
+
 } catch (error) {
     console.error(error.message);
 }
@@ -179,9 +126,9 @@ const newProductProps = {
 };
 
 try {
-    prodAdmin.editProduct(idProducto, newProductProps);
-    console.log("Resultado de editProduct(idProducto, newProductProps);")
-    console.log(prodAdmin.getProductById(idProducto))
+    const editedProduct = prodAdmin.editProduct(idProducto, newProductProps);
+    console.log("Resultado de: editProduct(idProducto, newProductProps)")
+    console.log(editedProduct);
 } catch (error) {
     console.error(error.message);
 }
@@ -193,7 +140,7 @@ const idProductoAEliminar = 1; // ID del producto agregado anteriormente
 
 try {
     prodAdmin.deleteProduct(idProductoAEliminar);
-    
+
 } catch (error) {
     console.error(error.message);
 }
