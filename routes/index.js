52bd1f2a3b385acb74405cedc3d8205050ee3a18
Tincho <tinchoreta@gmail.com>
2023-05-14 06:13:02 -0300

    Chat hbs ok

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import apiRouter from './api/index.js'
 import viewsRouter from './views/index.js'
+import chatRouter from './views/chat.js'
 
 const mainRouter = Router();
 
@@ -8,6 +9,6 @@ const mainRouter = Router();
 
 mainRouter.use('/api',apiRouter);
 mainRouter.use('/',viewsRouter);
-
+mainRouter.use('/chat',chatRouter);
 
 export default mainRouter;
--- routes/views/chat.js
+++ routes/views/chat.js
@@ -2,13 +2,16 @@ import { Router } from "express"
 const chatRouter = Router();
 
 
-chatRouter.get('/chat', async(req,res,next)=> {
+chatRouter.get('/', async(req,res,next)=> {
     try {
         return res.render('chat',{
             title: 'chat',
-            script: 'chat.js'
+            script: 'chat.js',
+            css: 'chat.css'
         });
     } catch(error) {
         next(error);
     }
 })
+
+export default chatRouter;
