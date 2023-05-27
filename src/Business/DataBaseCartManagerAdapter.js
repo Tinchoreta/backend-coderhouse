cebb50acc13e7049020dcd5d5d67d31dac103ba1
Tincho <tinchoreta@gmail.com>
2023-05-27 03:04:39 -0300

    DataBaseCartManagerAdapter added ok and Strategy updated ok

---
+++ src/Business/DataBaseCartManagerAdapter.js
@@ -0,0 +1,122 @@
+import PersistenceManager from '../Data/PersistenceManager.js';
+import DataBaseStrategy from '../Data/DataBaseStrategy.js';
+
+class DataBaseCartManagerAdapter {
+    static instance;
+
+    constructor(collectionName) {
+        if (DataBaseCartManagerAdapter.instance) {
+            throw new Error("Ya existe una instancia de esta clase");
+        }
+        this.PersistenceManager = new PersistenceManager(new DataBaseStrategy(collectionName));
+        DataBaseCartManagerAdapter.instance = this;
+    }
+
+    static getInstance(collectionName) {
+        if (!DataBaseCartManagerAdapter.instance) {
+            DataBaseCartManagerAdapter.instance = new DataBaseCartManagerAdapter(collectionName);
+        }
+        return DataBaseCartManagerAdapter.instance;
+    }
+
+    async getCarts() {
+        try {
+            const data = await this.PersistenceManager.load();
+            return data.map((cart) => cart.toJSON());
+        } catch (error) {
+            throw new Error(`getCarts: ${error.message}`);
+        }
+    }
+
+    async getProductsIds(cartId) {
+        try {
+            const cart = await this.PersistenceManager.getOne({ id: parseInt(cartId) });
+            return cart ? cart.products.map((product) => product.id) : [];
+        } catch (error) {
+            throw new Error(`getProductsIds: ${error.message}`);
+        }
+    }
+
+    async getCartById(cartId) {
+        try {
+            const cart = await this.PersistenceManager.getOne({ id: parseInt(cartId) });
+            return cart ? cart.toJSON() : null;
+        } catch (error) {
+            throw new Error(`getCartById: ${error.message}`);
+        }
+    };
+
+    async createCart() {
+        try {
+            const cart = { id: null, products: [] };
+            const createdCart = await this.PersistenceManager.addOne(cart);
+            return createdCart.id;
+        } catch (error) {
+            throw new Error(`createCart: ${error.message}`);
+        }
+    }
+
+    async updateCart(cartToUpdate) {
+        try {
+            const { id, products } = cartToUpdate;
+            const cartId = parseInt(id);
+
+            const cart = await this.PersistenceManager.getOne({ id: cartId });
+            if (!cart) {
+                throw new Error(`Cart with ID ${cartId} not found`);
+            }
+
+            const updatedCart = await this.PersistenceManager.modifyOne(
+                { id: cartId },
+                { ...cart, products }
+            );
+
+            return updatedCart.toJSON();
+        } catch (error) {
+            throw new Error(`updateCart: ${error.message}`);
+        }
+    }
+
+    async removeProductFromCart(cartId, productId) {
+        try {
+            const cart = await this.PersistenceManager.getOne({ id: parseInt(cartId) });
+            if (!cart) {
+                throw new Error(`Cart with ID: ${cartId} not found`);
+            }
+
+            const updatedProducts = cart.products.filter((product) => product.productId !== parseInt(productId));
+
+            const updatedCart = await this.PersistenceManager.modifyOne(
+                { id: cart.id },
+                { products: updatedProducts }
+            );
+
+            if (!updatedCart) {
+                throw new Error(`Product with ID ${productId} not found in cart ${cartId}`);
+            }
+
+            return updatedCart.toJSON();
+        } catch (error) {
+            throw new Error(`removeProductFromCart: ${error.message}`);
+        }
+    }
+
+    async deleteCart(idToDelete) {
+        try {
+            const id = Number(idToDelete);
+            if (isNaN(id)) {
+                throw new Error(`Cart ID "${idToDelete}" is not a valid number`);
+            }
+
+            const isDeleted = await this.PersistenceManager.deleteOne({ id });
+
+            if (!isDeleted) {
+                throw new Error(`Cart with ID: ${idToDelete} not found`);
+            }
+        } catch (error) {
+            throw new Error(`deleteCart: ${error.message}`);
+        }
+    }
+}
+
+export default DataBaseCartManagerAdapter;
--- src/Data/DataBaseStrategy.js
+++ src/Data/DataBaseStrategy.js
@@ -16,7 +16,7 @@ class DataBaseStrategy {
             console.log('Connected to MongoDB');
         } catch (error) {
             console.error('Failed to connect to MongoDB:', error);
-            throw error; 
+            throw error;
         }
     }
 
@@ -27,89 +27,89 @@ class DataBaseStrategy {
             console.log('Disconnected from MongoDB');
         } catch (error) {
             console.error('Failed to disconnect from MongoDB:', error);
-            throw error; 
+            throw error;
         }
     }
 
     async save(data) {
         try {
-            await this.model.create(data);
-            //console.log('Data saved to collection:', this.model.collection.collectionName);
+            const document = new this.model(data);
+            await document.save();
+            // console.log('Data saved to collection:', this.model.collection.collectionName);
         } catch (error) {
             console.error('Failed to save data:', error);
-            throw error; 
+            throw error;
         }
     }
 
     async load() {
         try {
             const data = await this.model.find({});
-            //console.log('Data loaded from collection:', this.model.collection.collectionName);
+            // console.log('Data loaded from collection:', this.model.collection.collectionName);
             return data;
         } catch (error) {
             console.error('Failed to load data:', error);
-            throw error; 
+            throw error;
         }
     }
 
     async delete() {
         try {
             await this.model.deleteMany({});
-            //console.log('Data deleted from collection:', this.model.collection.collectionName);
+            // console.log('Data deleted from collection:', this.model.collection.collectionName);
         } catch (error) {
             console.error('Failed to delete data:', error);
-            throw error; 
+            throw error;
         }
     }
 
     async getOne(filter) {
         try {
             const data = await this.model.findOne(filter);
-            //console.log('Retrieved document from collection:', this.model.collection.collectionName);
-            return data;
+            // console.log('Retrieved document from collection:', this.model.collection.collectionName);
+            return data ? data.toJSON() : null;
         } catch (error) {
             console.error('Failed to retrieve document:', error);
-            throw error; 
+            throw error;
         }
     }
 
     async deleteOne(filter) {
         try {
-            //// console.log(filter);
             const result = await this.model.deleteOne(filter);
-            //console.log('Deleted document from collection:', this.model.collection.collectionName);
-            
-            const isDeleted = result.deletedCount;
-            //// console.log(isDeleted + ' deleteOne strategy was called');
+            // console.log('Deleted document from collection:', this.model.collection.collectionName);
+
+            const isDeleted = result.deletedCount > 0;
+            // console.log(isDeleted + ' deleteOne strategy was called');
             return isDeleted;
         } catch (error) {
             console.error('Failed to delete document:', error);
-            throw error; 
+            throw error;
         }
     }
 
     async modifyOne(filter, update) {
         try {
             const result = await this.model.updateOne(filter, update);
-            //console.log('Modified document in collection:', this.model.collection.collectionName);
+            // console.log('Modified document in collection:', this.model.collection.collectionName);
             return result;
         } catch (error) {
             console.error('Failed to modify document:', error);
-            throw error; 
+            throw error;
         }
     }
 
     async addOne(data) {
         try {
-            const result = await this.model.create(data);
-            //console.log('Added document to collection: ', this.model.collection.collectionName);
-            return result;
+            const document = new this.model(data);
+            const result = await document.save();
+            // console.log('Added document to collection: ', this.model.collection.collectionName);
+            return result.toJSON();
         } catch (error) {
             console.error('Failed to add document:', error);
-            throw error; 
+            throw error;
         }
     }
 }
 
 export default DataBaseStrategy;
-
