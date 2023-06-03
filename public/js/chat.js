d8431ef6020800243fad3f67be5bf9b5da9129a9
Tincho <tinchoreta@gmail.com>
2023-06-03 12:10:08 -0300

    Chat enter with email

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,9 +8,9 @@ let btnSend = document.getElementById("btnSend");
 btnSend.addEventListener("click", handleSendMessage);
 
 Swal.fire({
-    title: "Write your name :)",
-    input: "text",
-    inputValidator: (value) => !value && "please write your name",
+    title: "Write your email :)",
+    input: 'email',
+    // inputValidator: (value) => !value && "Please write your email address",
     allowOutsideClick: false,
     icon: "success",
     background: "#767e87",
@@ -50,41 +50,3 @@ socket.on("allMessages", (message) => {
         .join("");
     scrollToBottom();
 });
-
-
-// socket.on("newMessage", (message) => {
-//     document.getElementById("chatMessages").innerHTML += `<br><b>${message.userName}:</b> ${message.message}`;
-// });
-
-
-    // // crea un nuevo elemento div para contener el mensaje y el nombre de usuario
-    // const chatMessage = document.createElement("div");
-    // chatMessage.classList.add("d-flex", "flex-row", "p-3");
-
-    // // crea una imagen de usuario y la agrega al mensaje
-    // const userImage = document.createElement("img");
-    // userImage.src = "https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png";
-    // userImage.width = 30;
-    // userImage.height = 30;
-    // chatMessage.appendChild(userImage);
-
-    // // crea un nuevo elemento div para el mensaje y lo agrega al mensaje
-    // const messageDiv = document.createElement("div");
-    // messageDiv.classList.add("ml-2", "p-3", "messages");
-    // messageDiv.innerHTML = `
-    //     <div class="d-flex flex-row p-3">
-    //         <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30"
-    //             height="30">
-    //         <div class="chat ml-2 p-3">
-    //             <b>${message.userName}:</b> ${message.message}
-    //         </div>
-    //     </div>
-    // `;
-    // chatMessage.appendChild(messageDiv);
-
-    // // Agrega el mensaje al contenedor de chatMessages
-    // const chatMessages = document.getElementById("chatMessages");
-    // chatMessages.appendChild(chatMessage);
-    // console.log(chatMessages.innerHTML + " " + chatMessages);
-    // // Mueve el foco al cuadro de chat para que el usuario pueda escribir un mensaje nuevo
-    // chatBox.focus();
