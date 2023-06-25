5349f5cd1a86b708ecac488e1a95453870c85460
Tincho <tinchoreta@gmail.com>
2023-06-25 18:29:01 -0300

    Adding customer routes api controller and adapter

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 import { Router } from "express"
 import authRouter from "./auth.router.js"
-import cartsRouter from "./customers.rocustomer.js"
+import cartsRouter from "./carts.router.js"
 import productsRouter from "./products.router.js"
 import sessionsRouter from "./sessions.router.js"
-import customersRouter from "./customer.router.js"
+import customersRouter from "./customers.router.js"
 
 const router = Router()
 
--- src/controllers/CustomerManagerController.js
+++ src/controllers/CustomerManagerController.js
@@ -138,3 +138,5 @@ class CustomerManagerController {
         }
     }
 }
+
+export default CustomerManagerController;
