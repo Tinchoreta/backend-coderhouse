842f3af19755c03451f0d9146e8c9b34387d44c4
Tincho <tinchoreta@gmail.com>
2023-10-18 07:51:07 -0300

    add customRouter to users ok

--- src/routes/api/sessions.router.js
+++ src/routes/api/sessions.router.js
@@ -1,10 +1,11 @@
-import { Router } from "express";
 import SessionsController from '../../controllers/SessionController.js';
+import CustomRouter from "../../middlewares/routes/CustomRouter.js";
+import ROLES from '../../utils/userRoles.js';
 
-const sessionsRouter = Router();
+const sessionsRouter = new CustomRouter();
 const sessionsController = new SessionsController();
 
 // Get session information
-sessionsRouter.get('/info', (req, res, next) => sessionsController.getSessionInfo(req, res, next));
+sessionsRouter.get('/info', [ROLES.ADMIN], (req, res, next) => sessionsController.getSessionInfo(req, res, next));
 
 export default sessionsRouter;
--- src/routes/api/users.router.js
+++ src/routes/api/users.router.js
@@ -1,4 +1,3 @@
-import { Router } from "express";
 import UserManagerController from "../../controllers/UserManagerController.js";
 import DataBaseUserAdapter from "../../Business/adapters/DataBaseUserAdapter.js";
 import {
@@ -8,8 +7,10 @@ import {
     validatePasswordLength,
     createHashForPassword
 } from "../../middlewares/business/userMiddleware.js";
+import CustomRouter from "../../middlewares/routes/CustomRouter.js";
+import ROLES from "../../utils/userRoles.js";
 
-const router = Router();
+const router = new CustomRouter();
 
 const dataBaseUserAdapter = DataBaseUserAdapter.getInstance(
     process.env.MONGO_DB_URI
@@ -17,18 +18,18 @@ const dataBaseUserAdapter = DataBaseUserAdapter.getInstance(
 
 const userController = new UserManagerController(dataBaseUserAdapter);
 
-router.get("/", (req, res) => userController.getUsers(req, res));
+router.get("/",[ROLES.ADMIN],  (req, res) => userController.getUsers(req, res));
 
-router.get("/:id", validateUserExistence, (req, res) => userController.getUserById(req, res));
+router.get("/:id", [ROLES.ADMIN], validateUserExistence, (req, res) => userController.getUserById(req, res));
 
-router.post("/",
+router.post("/", [ROLES.ADMIN],
     validateUserFields,
     checkDuplicateUserEmail,
     validatePasswordLength,
     createHashForPassword, (req, res) => userController.addUser(req, res));
 
-router.put("/:id", validateUserExistence, (req, res) => userController.updateUser(req, res));
+router.put("/:id", [ROLES.ADMIN], validateUserExistence, (req, res) => userController.updateUser(req, res));
 
-router.delete("/:id", validateUserExistence, (req, res) => userController.removeUser(req, res));
+router.delete("/:id", [ROLES.ADMIN], validateUserExistence, (req, res) => userController.removeUser(req, res));
 
 export default router;
