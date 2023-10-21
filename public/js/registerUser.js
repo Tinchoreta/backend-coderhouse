cd34764dab299c2f607b399f68de4fa80d8dc23b
Tincho <tinchoreta@gmail.com>
2023-10-21 19:03:05 -0300

    Mods on registerUser /login

--- public/js/registerUser.js
+++ public/js/registerUser.js
@@ -1,3 +1,66 @@
+const loginBtnModal = document.getElementById('loginBtnModal');
+const loginBtn = document.getElementById('loginBtn');
+const inputEmailModal = document.getElementById('inputEmail');
+const inputPasswordModal = document.getElementById('inputPassword');
+const inputEmail = document.getElementById('inputEmail1');
+const inputPassword = document.getElementById('inputPassword1');
+
+function handleModalLogin(event) {
+    event.preventDefault();
+    const email = inputEmailModal.value;
+    const password = inputPasswordModal.value;
+    authenticateUser(email, password);
+}
+
+function handleLogin(event) {
+    event.preventDefault();
+    const email = inputEmail.value;
+    const password = inputPassword.value;
+    authenticateUser(email, password);
+}
+
+loginBtnModal.addEventListener('click', handleModalLogin);
+loginBtn.addEventListener('click', handleLogin);
+
+async function authenticateUser(email, password) {
+    try {
+        const response = await fetch('http://localhost:8080/api/auth/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`,
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            sessionStorage.setItem('token', data.token);
+            sessionStorage.setItem('username', email);
+            window.location = '/products';
+            console.log("Login Success");
+        } else {
+            alert("Invalid Credentials");
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+        fetch.setDefaultHeaders({
+            'Authorization': `Bearer ${token}`,
+        });
+    }
+});
+
+
 document.getElementById('formAddUser').addEventListener('submit', function (event) {
     event.preventDefault();
 
